276d0e39415e5ca12ad33b71747af7d8f15c7dfb
harianchery <hari.anchery001@gmail.com>
2024-06-10 12:08:09 +0530

    view created

---
+++ src/components/View.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const View = () => {
+
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">name</th>
+                                    <th scope="col">friendName</th>
+                                    <th scope="col">friendNickName</th>
+                                    <th scope="col">Description</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default View
